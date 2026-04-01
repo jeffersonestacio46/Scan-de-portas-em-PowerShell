efbf21ad8f51d58e0914e2bc0ae1307dd3664d9e
Jefferson Oliveira <202501069592@alunos.estacio.br>
2026-04-01 00:42:12 -0300

    Add files via upload

---
+++ ScanMetasploitable2.ps1
@@ -0,0 +1,13 @@
+﻿#Scan para o Metasploitable2 
+# Solicita o IP do Metasploitable 2
+$ip = Read-Host "Digite o IP do Metasploitable 2"
+
+try {
+    foreach ($porta in 1..1024) {
+        if (Test-NetConnection $ip -Port $porta -WarningAction SilentlyContinue -InformationLevel Quiet) {
+            echo "Port $porta Opened"
+        }
+    }
+} catch {
+    echo "Ocorreu um erro durante a execução"
+}
